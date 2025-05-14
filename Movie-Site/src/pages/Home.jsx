ee5ddfa0b88f612262972ee0347c905d38cec97e
Maddz223 <lifelikedanny@gmail.com>
2025-05-14 12:16:30 +0100

    Add page components for Home.jsx

---
+++ Movie-Site/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold">Welcome to Movie Site</h1>
+    </div>
+  );
+};
+
+export default Home;
