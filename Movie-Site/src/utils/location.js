0dccc927a8432c014268d084d3a47fe37c2724aa
Maddz223 <lifelikedanny@gmail.com>
2025-05-14 17:30:34 +0100

    Add Location script to get users browser location.

---
+++ Movie-Site/src/utils/location.js
@@ -0,0 +1,7 @@
+const getCountryCode = () => {
+    const locale = Intl.DateTimeFormat().resolvedOptions().locale;
+    const match = locale.match(/-([A-Z]{2})$/i);
+    return match ? match[1] : 'US';
+};
+
+export { getCountryCode };
