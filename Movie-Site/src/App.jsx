2f7618b8f437629b0d0e77847204b26092d8a1d2
Maddz223 <lifelikedanny@gmail.com>
2025-05-14 13:24:28 +0100

    Add updated routes to add movie details route.

--- Movie-Site/src/App.jsx
+++ Movie-Site/src/App.jsx
@@ -1,21 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Movies from './pages/Movies';
-import TVSeries from './pages/TVSeries';
-import Watchlist from './pages/Watchlist';
-import ContactUs from './pages/ContactUs';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import TVSeries from "./pages/TVSeries";
+import Watchlist from "./pages/Watchlist";
+import ContactUs from "./pages/ContactUs";
+import Navbar from "./components/Navbar";
+import MovieDetails from "./pages/MovieDetails";
 
 function App() {
   return (
-    <Router> 
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/tv-series" element={<TVSeries />} />
-        <Route path="/watchlist" element={<Watchlist />} />
-        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/movies" element={<Movies />} />{/* Movies Route */}
+        <Route path="/tv-series" element={<TVSeries />} />{/* TV Series Route */}
+        <Route path="/watchlist" element={<Watchlist />} />{/* Watchlist Route */}
+        <Route path="/contact-us" element={<ContactUs />} />{/* ContractUS Route */}
+        <Route path="/movie/:movieId" element={<MovieDetails />} /> {/* Movie Details Route */}
       </Routes>
     </Router>
   );
