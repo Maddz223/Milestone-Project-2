c48a998d7e119ec14692c47256a2399c648e9713
Maddz223 <lifelikedanny@gmail.com>
2025-05-14 12:17:35 +0100

    Add page components for TVSeries.jsx

---
+++ Movie-Site/src/pages/TVSeries.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Card from '../components/Card';
+
+const TVSeries = () => {
+  const [tvSeries, setTvSeries] = useState([]);
+
+  useEffect(() => {
+    const fetchTVSeries = async () => {
+      const response = await axios.get('https://api.themoviedb.org/3/tv/popular?api_key=YOUR_API_KEY');
+      setTvSeries(response.data.results);
+    };
+    fetchTVSeries();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold mb-4">Popular TV Series</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {tvSeries.map((tv) => (
+          <Card key={tv.id} movie={tv} type="tv" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TVSeries;
