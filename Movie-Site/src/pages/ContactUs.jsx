7720a273fbcd2a21d4532af06f73f7b35ce25a07
Maddz223 <lifelikedanny@gmail.com>
2025-05-14 12:18:44 +0100

    Add page components for ContactUs.jsx

---
+++ Movie-Site/src/pages/ContactUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold">Contact Us</h1>
+      <p>If you have any questions, feel free to reach out!</p>
+    </div>
+  );
+};
