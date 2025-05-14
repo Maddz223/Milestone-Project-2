ecdd06184a3b7a481f44045b4e67206d46c44f31
Maddz223 <lifelikedanny@gmail.com>
2025-05-14 13:50:46 +0100

    Add Changed size on main heading to text-5xl

--- Movie-Site/src/pages/Home.jsx
+++ Movie-Site/src/pages/Home.jsx
@@ -92,7 +92,7 @@ const TrendingTV = () => {
 const Home = () => {
     return (
         <div>
-            <h1 className="text-4xl font-bold text-center my-8">
+            <h1 className="text-5xl font-bold text-center my-8">
                 Welcome to the Movie and TV Series App
             </h1>
             <TrendingMovies />
