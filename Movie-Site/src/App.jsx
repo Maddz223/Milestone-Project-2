4fd7d3c705f0de4e9ea72ee61a1d357ba0afe5f5
Maddz223 <lifelikedanny@gmail.com>
2025-05-15 20:46:44 +0100

    Add Lazy loading to help with performance.

--- Movie-Site/src/App.jsx
+++ Movie-Site/src/App.jsx
@@ -1,28 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Movies from "./pages/Movies";
-import TVSeries from "./pages/TVSeries";
-import Watchlist from "./pages/Watchlist";
-import ContactUs from "./pages/ContactUs";
-import Navbar from "./components/Navbar";
-import MovieDetails from "./pages/MovieDetails";
-import TVDetails from './pages/TVDetails';
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import SearchResults from './components/SearchResults';
+
+{/* Lazy imports to help with first loading */ }
+const Home = lazy(() => import('./pages/Home'));
+const Movies = lazy(() => import('./pages/Movies'));
+const TVSeries = lazy(() => import('./pages/TVSeries'));
+const Watchlist = lazy(() => import('./pages/Watchlist'));
+const ContactUs = lazy(() => import('./pages/ContactUs'));
+const MovieDetails = lazy(() => import('./pages/MovieDetails'));
+const TVDetails = lazy(() => import('./pages/TVDetails'));
+
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />{/* Movies Route */}
-        <Route path="/tv-series" element={<TVSeries />} />{/* TV Series Route */}
-        <Route path="/watchlist" element={<Watchlist />} />{/* Watchlist Route */}
-        <Route path="/contactus" element={<ContactUs />} />{/* ContractUS Route */}
-        <Route path="/movie/:movieId" element={<MovieDetails />} /> {/* Movie Details Route */}
-        <Route path="/tv/:tvId" element={<TVDetails />} />{/* TV Series Details Route */}
-        <Route path="/search" element={<SearchResults />} />{/* Search Results Route */}
-      </Routes>
+      <Suspense fallback={<div className="text-center p-4">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />{/* Movies Route */}
+          <Route path="/tv-series" element={<TVSeries />} />{/* TV Series Route */}
+          <Route path="/watchlist" element={<Watchlist />} />{/* Watchlist Route */}
+          <Route path="/contactus" element={<ContactUs />} />{/* ContractUS Route */}
+          <Route path="/movie/:movieId" element={<MovieDetails />} /> {/* Movie Details Route */}
+          <Route path="/tv/:tvId" element={<TVDetails />} />{/* TV Series Details Route */}
+          <Route path="/search" element={<SearchResults />} />{/* Search Results Route */}
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
