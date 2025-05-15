2cb63a9eaf155c1d1ab5ed2e4f0771ce33144b88
Maddz223 <lifelikedanny@gmail.com>
2025-05-16 00:43:40 +0100

    Add Applied footer and styling to keep to bottom of screen

--- Movie-Site/src/App.jsx
+++ Movie-Site/src/App.jsx
@@ -2,6 +2,7 @@ import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SearchResults from './components/SearchResults';
+import Footer from "./components/Footer";
 
 {/* Lazy imports to help with first loading */ }
 const Home = lazy(() => import('./pages/Home'));
@@ -13,22 +14,28 @@ const MovieDetails = lazy(() => import('./pages/MovieDetails'));
 const TVDetails = lazy(() => import('./pages/TVDetails'));
 
 
+
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Suspense fallback={<div className="text-center p-4">Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />{/* Movies Route */}
-          <Route path="/tv-series" element={<TVSeries />} />{/* TV Series Route */}
-          <Route path="/watchlist" element={<Watchlist />} />{/* Watchlist Route */}
-          <Route path="/contactus" element={<ContactUs />} />{/* ContractUS Route */}
-          <Route path="/movie/:movieId" element={<MovieDetails />} /> {/* Movie Details Route */}
-          <Route path="/tv/:tvId" element={<TVDetails />} />{/* TV Series Details Route */}
-          <Route path="/search" element={<SearchResults />} />{/* Search Results Route */}
-        </Routes>
-      </Suspense>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <Suspense fallback={<div className="text-center p-4">Loading...</div>}>
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movies" element={<Movies />} />{/* Movies Route */}
+              <Route path="/tv-series" element={<TVSeries />} />{/* TV Series Route */}
+              <Route path="/watchlist" element={<Watchlist />} />{/* Watchlist Route */}
+              <Route path="/contactus" element={<ContactUs />} />{/* ContractUS Route */}
+              <Route path="/movie/:movieId" element={<MovieDetails />} /> {/* Movie Details Route */}
+              <Route path="/tv/:tvId" element={<TVDetails />} />{/* TV Series Details Route */}
+              <Route path="/search" element={<SearchResults />} />{/* Search Results Route */}
+            </Routes>
+          </main>
+        </Suspense>
+        <Footer />
+      </div>
     </Router>
   );
 }
