f4397a685b9fb833da7e4e5fadff537cf3a48bba
Maddz223 <lifelikedanny@gmail.com>
2025-05-15 20:45:53 +0100

    Add tailwind-scrollbar-hide

--- Movie-Site/vite.config.js
+++ Movie-Site/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss() ],
-})
+  plugins: [
+    react(),
+    compression(),
+    tailwindcss(),
+  ],
+});
