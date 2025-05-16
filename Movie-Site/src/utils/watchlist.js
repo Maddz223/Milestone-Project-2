0577b5075917d6ec1476b0ec05cb1b264a779d40
Maddz223 <lifelikedanny@gmail.com>
2025-05-16 13:14:07 +0100

    Add Watchlist utility

---
+++ Movie-Site/src/utils/watchlist.js
@@ -0,0 +1,29 @@
+const WATCHLIST_KEY = "watchlist";
+
+export const getWatchlist = () => {
+  try {
+    return JSON.parse(localStorage.getItem(WATCHLIST_KEY)) || [];
+  } catch {
+    return [];
+  }
+};
+
+export const isInWatchlist = (id, type) => {
+  const list = getWatchlist();
+  return list.some((item) => item.id === id && item.type === type);
+};
+
+export const addToWatchlist = (item) => {
+  const list = getWatchlist();
+  if (!isInWatchlist(item.id, item.type)) {
+    list.push(item);
+    localStorage.setItem(WATCHLIST_KEY, JSON.stringify(list));
+  }
+};
+
+export const removeFromWatchlist = (id, type) => {
+  const updatedList = getWatchlist().filter(
+    (item) => !(item.id === id && item.type === type)
+  );
+  localStorage.setItem(WATCHLIST_KEY, JSON.stringify(updatedList));
+};
