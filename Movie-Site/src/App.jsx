3132754ffd86a68b3a0ef5cc3b677484cbeb5cbf
Maddz223 <lifelikedanny@gmail.com>
2025-05-16 12:24:42 +0100

    Add DarkMode to main app.jsx to pass to all pages.

--- Movie-Site/src/App.jsx
+++ Movie-Site/src/App.jsx
@@ -13,12 +13,10 @@ const ContactUs = lazy(() => import('./pages/ContactUs'));
 const MovieDetails = lazy(() => import('./pages/MovieDetails'));
 const TVDetails = lazy(() => import('./pages/TVDetails'));
 
-
-
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
         <Navbar />
         <Suspense fallback={<div className="text-center p-4">Loading...</div>}>
           <main className="flex-grow">
@@ -28,7 +26,7 @@ function App() {
               <Route path="/tv-series" element={<TVSeries />} />{/* TV Series Route */}
               <Route path="/watchlist" element={<Watchlist />} />{/* Watchlist Route */}
               <Route path="/contactus" element={<ContactUs />} />{/* ContractUS Route */}
-              <Route path="/movie/:movieId" element={<MovieDetails />} /> {/* Movie Details Route */}
+              <Route path="/movie/:movieId" element={<MovieDetails />} />{/* Movie Details Route */}
               <Route path="/tv/:tvId" element={<TVDetails />} />{/* TV Series Details Route */}
               <Route path="/search" element={<SearchResults />} />{/* Search Results Route */}
             </Routes>
