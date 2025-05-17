82636634176bc8f8d326431bbc403a6a1edf68ba
Maddz223 <lifelikedanny@gmail.com>
2025-05-17 11:30:47 +0100

    Add context import, comments to code for better reading

--- Movie-Site/src/App.jsx
+++ Movie-Site/src/App.jsx
@@ -1,10 +1,16 @@
-import { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import SearchResults from './components/SearchResults';
-import Footer from "./components/Footer";
+import { Suspense, lazy } from 'react'; // To help with preformance
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Route handler
 
-{/* Lazy imports to help with first loading */ }
+// Context
+import { WatchlistProvider } from "./context/WatchlistContext";
+
+// Components
+import Navbar from './components/Navbar'; // Navbar import
+import SearchResults from './components/SearchResults'; // SearchResults import
+import Footer from "./components/Footer"; // Footer import
+
+
+// Lazy Imports to help with performance
 const Home = lazy(() => import('./pages/Home'));
 const Movies = lazy(() => import('./pages/Movies'));
 const TVSeries = lazy(() => import('./pages/TVSeries'));
@@ -15,26 +21,28 @@ const TVDetails = lazy(() => import('./pages/TVDetails'));
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
-        <Navbar />
-        <Suspense fallback={<div className="text-center p-4">Loading...</div>}>
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/movies" element={<Movies />} />{/* Movies Route */}
-              <Route path="/tv-series" element={<TVSeries />} />{/* TV Series Route */}
-              <Route path="/watchlist" element={<Watchlist />} />{/* Watchlist Route */}
-              <Route path="/contactus" element={<ContactUs />} />{/* ContractUS Route */}
-              <Route path="/movie/:movieId" element={<MovieDetails />} />{/* Movie Details Route */}
-              <Route path="/tv/:tvId" element={<TVDetails />} />{/* TV Series Details Route */}
-              <Route path="/search" element={<SearchResults />} />{/* Search Results Route */}
-            </Routes>
-          </main>
-        </Suspense>
-        <Footer />
-      </div>
-    </Router>
+    <WatchlistProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
+          <Navbar />
+          <Suspense fallback={<div className="text-center p-4">Loading...</div>}>
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/tv-series" element={<TVSeries />} />
+                <Route path="/watchlist" element={<Watchlist />} />
+                <Route path="/contactus" element={<ContactUs />} />
+                <Route path="/movie/:movieId" element={<MovieDetails />} />
+                <Route path="/tv/:tvId" element={<TVDetails />} />
+                <Route path="/search" element={<SearchResults />} />
+              </Routes>
+            </main>
+          </Suspense>
+          <Footer />
+        </div>
+      </Router>
+    </WatchlistProvider>
   );
 }
 
