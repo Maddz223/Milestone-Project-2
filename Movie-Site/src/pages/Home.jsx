5c57e4f3ada93633dae5648ce8eb965666508c37
Maddz223 <lifelikedanny@gmail.com>
2025-05-17 14:04:33 +0100

    Add latesttrailers to home page.

--- Movie-Site/src/pages/Home.jsx
+++ Movie-Site/src/pages/Home.jsx
@@ -1,5 +1,7 @@
+// Components
 import PopularTV from "../components/PopularTV";
 import PopularMovies from "../components/PopularMovies";
+import LatestTrailers from "../components/LatestTrailers";
 
 // Home Component
 const Home = () => {
@@ -8,6 +10,7 @@ const Home = () => {
             <h1 className="text-5xl font-bold text-center my-8">
                 Welcome to the Movie and TV Series App
             </h1>
+            <LatestTrailers />
             <PopularMovies />
             <PopularTV />
         </div>
