0cc52f23753a94145cad094b09e95fb2886dfb3a
Maddz223 <lifelikedanny@gmail.com>
2025-05-19 20:08:37 +0100

    Add import metsetupTests.js

--- Movie-Site/src/setupTests.js
+++ Movie-Site/src/setupTests.js
@@ -1,1 +1,11 @@
 require('@testing-library/jest-dom');
+Object.defineProperty(global, "importMeta", {
+    value: {
+        env: {
+            VITE_TMDB_API_KEY: "test_api_key",
+            // add other env variables if needed
+        },
+    },
+});
+
+global.import = { meta: global.importMeta };
