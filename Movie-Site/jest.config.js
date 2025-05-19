85f9b9e4babe50944fbb9d849f10ef416e2d1237
Maddz223 <lifelikedanny@gmail.com>
2025-05-19 19:48:10 +0100

    Fix jest.config.js and add jest.setup.js

--- Movie-Site/jest.config.js
+++ Movie-Site/jest.config.js
@@ -1,12 +1,11 @@
-// jest.config.js
 export default {
-      testEnvironment: 'jsdom',
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
-    moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-    setupFilesAfterEnv: ['./src/setupTests.js'],
-    moduleFileExtensions: ['js', 'jsx'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['./src/setupTests.js', './jest.setup.js'],
+  moduleFileExtensions: ['js', 'jsx'],
 };
