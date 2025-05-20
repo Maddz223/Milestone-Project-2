853b0fadee099c25dcb1ff65e5bccdbf67123219
Maddz223 <lifelikedanny@gmail.com>
2025-05-20 12:46:05 +0100

    Add Gh-pages package and steps to deploy gh-pages branch

--- Movie-Site/vite.config.js
+++ Movie-Site/vite.config.js
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import compression from 'vite-plugin-compression';
+import viteCompression from 'vite-plugin-compression';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Milestone-Project-2/',
   plugins: [
     react(),
-    compression(),
+    viteCompression({
+      algorithm: 'gzip', // or 'brotliCompress'
+      ext: '.gz', // output extension
+      deleteOriginFile: false, // keep original files too
+    }),
+    createHtmlPlugin({ minify: true }),
     tailwindcss(),
   ],
+  build: {
+    minify: "esbuild",
+    sourcemap: false,
+    target: "esnext",
+  },
+  server: {
+    compress: true, // for gzip compression
+  },
 });
