3cfbb1703386b642dd960dc604090e5f7d9ef71f
Maddz223 <lifelikedanny@gmail.com>
2025-05-20 13:26:59 +0100

    Edit reverted to Hashrouter.

--- Movie-Site/src/App.jsx
+++ Movie-Site/src/App.jsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Context
 import { WatchlistProvider } from "./context/WatchlistContext";
