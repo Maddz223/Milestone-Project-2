6398a7a60d85c8560c03e7b0dc1922aeaee2c31f
Maddz223 <lifelikedanny@gmail.com>
2025-05-20 13:52:01 +0100

    Edit Added more contrast between the watchlist button and the background

--- Movie-Site/src/components/WatchlistButton.jsx
+++ Movie-Site/src/components/WatchlistButton.jsx
@@ -4,7 +4,7 @@ const WatchlistButton = ({ inWatchlist, onToggle }) => {
     <button
       onClick={onToggle}
       className={`text-sm font-semibold text-white px-4 py-2 rounded transition-colors duration-300 
-      ${inWatchlist ? "bg-red-600 hover:bg-red-700" : "bg-blue-600 hover:bg-blue-700"}`}>
+      ${inWatchlist ? "bg-red-600 hover:bg-red-700" : "bg-blue-700 hover:bg-blue-800"}`}>
       {inWatchlist ? "Remove from Watchlist" : "Add to Watchlist"}
     </button>
   );
